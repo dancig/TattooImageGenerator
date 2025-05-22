f43d432de9939ecf0796a788dc9c37b4c2c95fa9
dancig <ya.kutlanov@yandex.ru>
2025-05-22 16:16:56 +0300

    feat: add language detection

--- Translator.py
+++ Translator.py
@@ -6,13 +6,17 @@ class TranslatorModule:
 
     # Функция перевода текста на английский язык
     # Принимает строку текстового запроса
-    # Возвращает переведенную на английский язык строку
+    # Возвращает переведенную на английский язык строку, если она написана не на английском языке,
+    # Иначе возвращает строку без изменений
     async def translate(self, text):
         async with Translator() as translator:
-            return (await translator.translate(text=text, src='ru', dest='en')).text
+            if (await translator.detect(text)).lang == 'en':
+                return text
+            else:
+                return (await translator.translate(text=text, src='ru', dest='en')).text
+
     # Функция запуска перевода
     # Принимает строку текстового запроса
     # Запускает перевод и возвращает строку переведенную на английский язык
     def translate_text(self, text):
         return asyncio.run(self.translate(text))
-
