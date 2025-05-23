620e8060986992c2927c50c68c1d076c362c001c
dancig <ya.kutlanov@yandex.ru>
2025-05-23 16:14:50 +0300

    feat: add error handle in server.py and download.js

--- Server.py
+++ Server.py
@@ -3,6 +3,7 @@ from ImageGenerator import ImageGenerator
 from Translator import TranslatorModule
 import os
 import time
+import traceback
 
 app = Flask(__name__)
 images_folder = os.path.join('static', 'images')
@@ -13,36 +14,47 @@ translator = TranslatorModule()
 
 # Функция обработки запросов
 # Возвращает страницу "base.html", если запрос "GET"
-# Если запрос "POST", текст запроса существует, его длина больше 0 и меньше или равна 500
-# и модуль генерации изображений не занят генерацией, то текстовый запрос переводится на английский,
-# генерируется и сохраняется изображение, возвращается страница "base.html" с аргументами:
-# last_prompt = последний текстовый запрос, image =  путь к сгенерированному изображению
-# Если запрос "POST", текстовый запрос прошел валидацию и модуль генерации изображений занят генерацией,
-# то возвращается страница "base.html" с аргументами: last_prompt = последний текстовый запрос и isBusy = True
-# Если запрос "POST" и текстовый запрос не прошел валидацию, то возвращается страница "base.html" с аргументами:
-# last_prompt = пустая строка, isBadPrompt = True
+# Если метод запроса не "POST", то возвращается шаблон "base.html"
+# Если текстовой строки нет в запросе или длина строки превышает 500 символов или длина меньше или равна нулю, то
+# возвращается шаблон "base.html" с аргументами last_prompt = "" и isBadPrompt=True
+# Если модуль генерации изображений занят, то возвращается шаблон "base.html" с аргументами last_prompt=текстовый запрос
+# и isBusy = True
+# Если метод запроса "POST", текст запроса прошел валидацию и модуль генерации изображений не занят, то запускается
+# перевод текстового запроса на английский, запускается генерация изображения по переведенному тексту,
+# изображение сохраняется с именем файла - дата и время завершения генерации, возвращается шаблон "base.html"
+# с аргументами last_prompt = текстовый запрос, image = путь до сгенерированного изображения
+# Если происходят ошибки, то возвращается шаблон "base.html" с аргументами last_prompt = "" и error = True,
+# текст ошибки выводится в консоль
 @app.route("/", methods=['GET', 'POST'])
 def process_requests():
-    if request.method == 'POST':
-        if ("prompt" in request.form) and (len(request.form["prompt"]) <= 500) and (len(request.form["prompt"]) > 0):
-            if imageGen.isBusy == False:  # Запрос "POST" и модуль генерации не занят
-                imageGen.isBusy = True
-                prompt = request.form["prompt"]
-                translated_prompt = translator.translate_text(prompt)  # Перевод текстового запроса на английский
-                print(translated_prompt)
-                image = imageGen.generate_image(translated_prompt)  # Генерация изображения по текстовому запросу
-                filename = time.strftime("%Y_%m_%d_%H%M%S", time.localtime(time.time()))  # Название файла -
-                                                                                          # текущая дата и время
-                image.save(f"static/images/{filename}.png")  # Сохранение сгенерированного изображения
-                saved_image = os.path.join(app.config["UPLOAD_FOLDER"], f"{filename}.png")
-                imageGen.isBusy = False
-                return render_template('base.html', last_prompt=prompt, image=saved_image)
-            else:
-                return render_template('base.html', last_prompt=request.form["prompt"], isBusy=True)
-        else:
-            return render_template('base.html', last_prompt="", isBadPrompt=True)
-    else:
-        return render_template('base.html')
+    try:
+        if request.method != 'POST':
+            return render_template('base.html')     # Если метод не "POST"
+        if ("prompt" not in request.form) or (len(request.form["prompt"]) > 500) or (len(request.form["prompt"]) <= 0):
+            return render_template('base.html', last_prompt="", isBadPrompt=True)   # Если текстовый запрос
+                                                                                    # не прошел валидацию
+        if imageGen.isBusy == True:
+            return render_template('base.html', last_prompt=request.form["prompt"], isBusy=True)    # Если модуль
+                                                                                        # генерации изображений занят
+        try:
+            imageGen.isBusy = True
+            prompt = request.form["prompt"]
+            translated_prompt = translator.translate_text(prompt)  # Перевод текстового запроса на английский
+            print(translated_prompt)
+            image = imageGen.generate_image(translated_prompt)  # Генерация изображения по текстовому запросу
+            filename = time.strftime("%Y_%m_%d_%H%M%S", time.localtime(time.time()))    # Название файла -
+                                                                                        # текущая дата и время
+            image.save(f"static/images/{filename}.png")  # Сохранение сгенерированного изображения
+            saved_image = os.path.join(app.config["UPLOAD_FOLDER"], f"{filename}.png")
+            imageGen.isBusy = False
+            return render_template('base.html', last_prompt=prompt, image=saved_image)
+        except Exception:   # Ошибка произошла на этапе обработки запроса
+            traceback.print_exc()
+            imageGen.isBusy = False
+            return render_template('base.html', last_prompt="", error=True)
+    except Exception:   # Ошибка произошла на этапе проверки запроса
+        traceback.print_exc()
+        return render_template('base.html', last_prompt="", error=True)
 
 
 if __name__ == "__main__":
--- static/download.js
+++ static/download.js
@@ -1,12 +1,20 @@
 // Функция сохранения сгенерированного изображения
 // Сохраняет сгенерированное изображение на устройство пользователя
+// При возвникновении ошибки выводит на экран сообщении об ошибке
 function downloadImage() {
-    const link = document.createElement('a');
-    link.href = document.getElementById("img").src;         // Путь к сгенерированному изображению
-    link.download = document.getElementById("img").src;     // Имя сохраняемого файла
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    const path = document.getElementById("img").src;
+    fetch(path).then(response => {  // Проверка существования файла
+        if (response.ok) {          // Файл существует
+            const link = document.createElement('a');
+            link.href = path;         // Путь к сгенерированному изображению
+            link.download = path;     // Имя сохраняемого файла
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+        else
+            alert("Произошла ошибка при сохранении файла."); // Файл не существует
+    })
 }
 
 const btn = document.getElementById("btn-dl");
